c399007ea3612bd4868c42fdff04e6fbcbc184c7
foagura <foagura_0416@yahoo.co.jp>
2024-09-09 22:00:33 +0900

    追加

--- src/Practice12.java
+++ src/Practice12.java
@@ -25,5 +25,17 @@ public class Practice12 {
             default :
                 System.out.println("default実行完了");
         }
+
+        int c = 20;
+
+        switch (c) {
+            default :
+                System.out.println("default実行完了");  // caseに該当するものが無い為defaultが実行される
+            case 1:
+                System.out.println("aの値は1です");  // defaultにbreakが無い為、その次に記述されているcase 1:も実行される
+                break;
+            case 2:
+                System.out.println("aの値は2です");  // case 1:にbreakがある為、case 2:は実行されない
+        }
     }
 }
